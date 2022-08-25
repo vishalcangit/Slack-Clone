842dde08394ef0e65180402f8fbb4708998b08a6
vishalcangit <vishalsuvarna000@gmail.com>
2022-08-25 15:41:35 +0530

    slack project started

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,94 @@
+import TimeIcon from "@mui/icons-material/AccessTime";
+import Avatar from "@mui/icons-material/AccountCircle";
+import HelpIcon from "@mui/icons-material/Help";
+import SearchIcon from "@mui/icons-material/Search";
+import React from "react";
+import styled from "styled-components";
+
+function Header() {
+  return (
+    <HeaderContainer>
+      {/* header left */}
+      <HeaderLeft>
+        <HeaderAvatar
+        // todo onclick
+        />
+        <HeaderTimeIcon />
+      </HeaderLeft>
+
+      {/* header mid */}
+      <HeaderSearch>
+        <SearchIcon />
+        <input placeholder="Search message" />
+      </HeaderSearch>
+
+      {/* header right */}
+      <HeaderRight>
+        <HelpIcon />
+      </HeaderRight>
+    </HeaderContainer>
+  );
+}
+
+export default Header;
+
+const HeaderContainer = styled.div`
+  display: flex;
+  position: fixed;
+  width: 100%;
+  align-items: center;
+  justify-content: space-between;
+  padding: 10px 0;
+  background-color: var(--slack-color);
+  color: white;
+`;
+const HeaderLeft = styled.div`
+  flex: 0.3;
+  display: flex;
+  align-items: center;
+  margin-left: 20px;
+`;
+const HeaderAvatar = styled(Avatar)`
+  cursor: pointer;
+  font-size: 3rem !important;
+  :hover {
+    opacity: 0.8;
+  }
+`;
+const HeaderTimeIcon = styled(TimeIcon)`
+  margin-left: 10vw;
+  margin-right: 30px;
+`;
+
+// mid header
+const HeaderSearch = styled.div`
+  flex: 0.4;
+  opacity: 1;
+  border-radius: 6px;
+  background-color: #421f44;
+  text-align: center;
+  display: flex;
+  padding: 0 50px;
+  color: gray;
+  border: 1px solid gray;
+
+  > input {
+    background-color: transparent;
+    border: none;
+    text-align: center;
+    min-width: 30vw;
+    outline: none;
+    color: white;
+  }
+`;
+// header right
+const HeaderRight = styled.div`
+  flex: 0.3;
+  display: flex;
+  align-items: flex-end;
+
+  > .MuiSvgIcon-root {
+    margin-left: auto;
+    margin-right: 50px;
+  }
+`;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBD5do5KYlw_edxMqzPvKaq3dmuCGoL3tU",
+  authDomain: "slack-react-64aa6.firebaseapp.com",
+  projectId: "slack-react-64aa6",
+  storageBucket: "slack-react-64aa6.appspot.com",
+  messagingSenderId: "149423326907",
+  appId: "1:149423326907:web:230fe4a6806dfa91fd0b9e",
+  measurementId: "G-SXWVRD35SD",
+};
